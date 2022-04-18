15c73933d8ee33950d90f30e2ba2ba5df8e3a8e6
myself <has2356@outlook.com>
2022-04-18 21:53:45 +0800

    了解req请求对象

---
+++ 33天/了解req请求对象.js
@@ -0,0 +1,27 @@
+const http = require('http');
+
+const fs = require('fs');
+
+const path = require('path');
+
+const server = http.createServer();
+
+server.on('request',(req,res) => {
+    let url = req.url;
+    let str = '<h1>404</h1>'
+    res.setHeader('Content-Type','text/html;charset=utf-8')
+    if (url == '/index.html' || url == '/') {
+        fs.readFile(path.join(__dirname,'/index.html'),'utf-8',function (re,data) {
+            //console.log(data);
+            res.end(data)
+        })
+    }else{
+        res.end(str);
+    }
+
+    
+});
+
+server.listen('8080',(req) =>{
+    console.log(req);
+})
