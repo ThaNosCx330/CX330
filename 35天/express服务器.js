0b98176833feebdea43cf0e69e6481938ab42bb6
myself <has2356@outlook.com>
2022-04-20 19:09:57 +0800

    express路由课程

--- 35天/express服务器.js
+++ 35天/express服务器.js
@@ -7,19 +7,16 @@ const app = express();
 
 //响应数据
 
-app.get('/user',(req,res) => {
-
-    res.send({
-        data:123,
-    })
+app.get('/:id/:name',(req,res) => {
+    console.log(req.query);
+    res.send(req.params)
 })
 
 app.post('/user',(req,res) => {
 
-    console.log(req);
+
     res.send('POST成功')
 })
-
 
 //启动服务器
 
