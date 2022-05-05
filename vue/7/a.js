21526330aa59d5f433e63aad3336528b78e231d1
myself <has2356@outlook.com>
2022-05-05 17:16:54 +0800

    ok

---
+++ vue/7/a.js
@@ -0,0 +1,4 @@
+import i, { c as cc}from './index.js'
+
+console.log(i);
+console.log(cc);
---
+++ vue/7/index.js
@@ -0,0 +1,17 @@
+let a = 10;
+
+let b = 20;
+
+
+
+function fn() {
+    
+}
+
+export let c = 30;
+
+export default {
+    a,
+    b,
+    fn,
+}
